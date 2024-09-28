a494ebf8725f8829687a478133010af5e5533db6
Jcoll05 <jcolla@uninorte.edu.co>
2024-09-27 20:05:03 -0500

    Loaded featured products and plp contents into the API.

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const promotional = require("./data/promotional");
+const featured = require("./data/featured");
+const product = require("./data/product");
 
 module.exports = () => {
   return {
     promotional: promotional,
+    featured: featured,
+    product: product,
   };
 };
