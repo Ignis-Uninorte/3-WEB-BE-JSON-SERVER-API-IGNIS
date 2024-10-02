5e7eac1b6841d07936b1b16ad6b37548cc07125f
Jcoll05 <jcolla@uninorte.edu.co>
2024-10-02 14:51:32 -0500

    changed image url.

--- data/promotional.js
+++ data/promotional.js
@@ -1,6 +1,6 @@
 const promotional = [
 
-      { "id": 1, "imageUrl": "../assets/Discounted_product1.jpg", "altText": "Promotion 1" },
+      { "id": 1, "imageUrl": "https://three-web-be-json-server-api-ignis.onrender.com/assets/Discounted_product1.jpg", "altText": "Promotion 1" },
       { "id": 2, "imageUrl": "../assets/Discounted_product2.jpg", "altText": "Promotion 2" }
 ];
 
